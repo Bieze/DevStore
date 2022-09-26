311cbbcb5494f320acc8786a01515fd43a85bd75
Abdul <abdul@abdulhani.com>
2022-09-25 22:00:31 -0500

    Base project added

---
+++ devstore/__init__.py
@@ -0,0 +1,11 @@
+"""
+A utility tool for programmers
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2022-present abdulh4ni
+:license: GPL v3, see LICENSE for more details
+
+author: Abdul A. <abdul@abdulhani.com>
+github: https://github.com/abdulh4ni/DevStore
+git: https://www.abdulhani.com/DevStore.git/
+"""
---
+++ devstore/__starter__.py
@@ -0,0 +1,5 @@
+from utils import sqlite3
+
+
+if __name__ == "__main__":
+    sqlite3.Database()
